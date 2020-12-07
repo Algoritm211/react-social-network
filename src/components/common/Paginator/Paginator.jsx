2fc0188cb50ce5a2c9b68f2f2c5373655cdeef83
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-12-07 12:46:04 +0200

    Add more logic on paginator component, refactor this element

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,31 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './Paginator.module.css'
 
 
-const Paginator = ({totalUsersCount, currentPage, usersPerPage, onChangePage}) => {
-  const countUsers = Math.ceil(totalUsersCount / usersPerPage)
+const Paginator = ({totalItemsCount, currentPage, usersPerPage, onChangePage, portionSize = 10}) => {
+
+
+  const pagesCount = Math.ceil((totalItemsCount / usersPerPage))
+
+  const portionCount = Math.ceil(pagesCount / portionSize) // const
+  const [portionNumber, setPortionNumber] = useState(1)
+  const pageLeftBorder = (portionNumber - 1) * portionSize + 1
+  const pageRightBorder = portionNumber * portionSize
 
   let pages = []
-  for (let i = 1; i <= countUsers; i++) {
+  for (let i = 1; i <= pagesCount; i++) {
     pages.push(i)
   }
 
-  let paginatorElement = pages.map(item => {
-    let activeClass = currentPage === item ? classes.activePageItem : ''
-    return (
-      <div 
-        key={item} 
-        className={`${classes.pageItem} ${activeClass}`} 
-        onClick={() => onChangePage(item)}
-        >
-          <div>{item}</div>
-        </div> 
-    )
-  })
+  let paginatorElement = pages
+    .filter(pageNumber => {
+      return pageNumber >= pageLeftBorder && pageNumber <= pageRightBorder
+    })
+    .map((item) => {
+      let activeClass = currentPage === item ? classes.activePageItem : ''
+      return (
+        <React.Fragment>
+          <div
+            key={ item }
+            className={ `${ classes.pageItem } ${ activeClass }` }
+            onClick={ () => onChangePage(item) }
+          >
+            <div>
+              { item }
+            </div>
+          </div>
+        </React.Fragment>
+      )
+    })
 
   return (
     <React.Fragment>
-      {paginatorElement}
+      { portionNumber > 1 &&
+      <button onClick={ () => setPortionNumber(portionNumber - 1) }>PREV</button>
+      }
+      { paginatorElement }
+      { portionCount > portionNumber &&
+      <button onClick={ () => setPortionNumber(portionNumber + 1) }>NEXT</button> }
     </React.Fragment>
   )
 }
